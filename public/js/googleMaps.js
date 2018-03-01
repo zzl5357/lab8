9abbcfcbe29ff4f2dabe1a22b8cd2877ec624e4d
Ziliang Zhang <ziz137@ucsd.edu>
2018-02-28 21:44:48 -0800

    lab8 push

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.880, lng:-117.236};
+
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title:"UCSD"
+	});
+
 }
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,6 +18,16 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:"+targetIDPrefix);
+			$("#"+targetIDPrefix+"-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
